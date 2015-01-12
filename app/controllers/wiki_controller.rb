774346ef35baec6077fd7e4be776578fbeee07ac
Torsten Ruger <torsten@villataika.fi>
2015-01-12 23:28:17 +0200

    new page coming up, but quite empty

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -10,7 +10,14 @@ class WikiController < ApplicationController
     @pages = WikiPage.wiki.pages
   end
 
+  def page
+  end
 
   private
+
+  def find_page
+    @page = WikiPage.find(params[:page])
+    redirect_to new_page_path(title: params[:page]), notice: t(:notice_page_does_to_exist) unless @page
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@
 
 GollumRails::Engine.routes.draw do
 
+  get  "/index" => "wiki#index", as: :wiki_root
+  get "/new" => "wiki#new" , :as => :new_wiki_page
+
   constraints(:folder => /.*/, :page => /.*/ ) do
-    get  "/index" => "wiki#index", as: :wiki_root
+    get ":page" => "wiki#page" , :as => :wiki_page
   end
 
 end
