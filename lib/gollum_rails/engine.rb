090c6b808c1a9ef3a300cce11a039f059155dfff
Torsten Ruger <torsten@villataika.fi>
2015-01-16 23:27:57 +0200

    use locales

--- lib/gollum_rails/engine.rb
+++ lib/gollum_rails/engine.rb
@@ -3,13 +3,10 @@ module GollumRails
   class Engine < ::Rails::Engine
     engine_name "gollum_rails"
 
-    initializer "gollum_rails.assets.precompile" do |app|
-#      app.config.assets.precompile += %w(office_clerk.css office_clerk.js)
-    end
+    config.i18n.default_locale = :en
 
-    # for testing this makes engine factories available
-    initializer "model_core.factories", :after => "factory_girl.set_factory_paths" do
-      FactoryGirl.definition_file_paths << File.expand_path('../../../spec/factories', __FILE__) if defined?(FactoryGirl)
+    initializer "gollum_rails.assets.precompile" do |app|
+      app.config.assets.precompile += %w(gollum_rails.css gollum_rails.js)
     end
 
     config.after_initialize do
