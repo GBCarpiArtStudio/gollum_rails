5f25558421cfa64228acaca2de2c69df54227014
Torsten Ruger <torsten@villataika.fi>
2015-01-17 12:24:10 +0200

    little less fluff

--- lib/gollum_rails/path_helpers.rb
+++
@@ -1,8 +0,0 @@
-module GollumRails
-  module PathHelpers
-    def self.configure_path(path)
-      return path if path.starts_with?("/")
-      Rails.root.join(path)
-    end
-  end
-end
--- spec/unit/version_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'gollum_rails/version'
-
-describe GollumRails::VERSION do
-  it "should have version 0.0.1" do
-    expect(GollumRails::VERSION).to match "0.0.1"
-  end
-end
