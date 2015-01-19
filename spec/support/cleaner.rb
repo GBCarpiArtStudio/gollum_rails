e42e6dca9c54f1c1b40d78384a9377af20f15d48
Torsten Ruger <torsten@villataika.fi>
2015-01-19 21:17:46 +0200

    clean after context
    just have to keep it in mind, each context is dependent

--- spec/support/cleaner.rb
+++ spec/support/cleaner.rb
@@ -1,9 +1,9 @@
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before(:context) do
     WikiPage.init_wiki
   end
-  config.after(:each) do
+  config.after(:context) do
     FileUtils.rm_r(WikiPage.wiki.path.to_s) if WikiPage.wiki.path.exist?
   end
 end
